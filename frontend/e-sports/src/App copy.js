ef17e49ce7933d3bd0f6b0742429aa1a13b90f3a
girlany <68343804+girlany@users.noreply.github.com>
2023-02-11 23:18:13 -0400

    front

---
+++ frontend/e-sports/src/App copy.js
@@ -0,0 +1,55 @@
+
+
+
+import { BrowserRouter as Router, Routes, 
+  Route } from "react-router-dom";
+import About from "./pages/About";
+import About2 from "./pages/About2";
+
+import MyButton from "./components/Button";
+
+import './teste.css';
+
+
+const App = () => {
+    return (
+  
+        <div className="teste2">
+
+<div className="teste">
+
+<Router>
+    <MyButton id="main-button" to="" />
+    <MyButton  to="about2" />
+    <MyButton  to="about" />
+    <Routes>
+   
+     
+        <Route path="/about" 
+            element={<About />} />
+
+              <Route path="/about2" 
+            element={<About2 />} />
+
+
+
+
+    </Routes>
+</Router>
+
+
+</div>
+               
+               <h1 className="hh1">Registro E-sports</h1>
+               
+        </div>
+       
+
+
+    
+
+       
+    )
+}
+
+export default App;
